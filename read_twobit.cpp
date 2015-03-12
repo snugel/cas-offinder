7f97bbbff4445662e7ae81604ad156db5d78fc46
Jeongbin Park <pjb7687@gmail.com>
2015-03-12 16:01:34 +0900

    Implementation 2

--- read_twobit.cpp
+++ read_twobit.cpp
@@ -1,20 +1,20 @@
 #include "read_twobit.h"
 
-using namespace std;
+#include <iostream>
+#include <utility>
 
-typedef union {
-	char bytes[4];
-	unsigned int num;
-} record;
+#include <cstdlib>
+
+using namespace std;
 
 typedef union {
 	unsigned char bytes[4];
 	unsigned int num;
 } RECORD4;
 
-unsigned int read_uint(ifstream &input) {
+unsigned int read_uint(FILE *input) {
 	static RECORD4 val4;
-	input.read((char*)val4.bytes, 4);
+	fread((char*)val4.bytes, 4, 1, input);
 	return val4.num;
 }
 
@@ -28,21 +28,66 @@ char bit_to_seq(unsigned char b) {
 		return 'A';
 	case 3:
 		return 'G';
+	}
 }
 
 int read_twobit(string &filepath, vector<string> &chrnames, string &content, vector<unsigned long long> &chrpos) {
-	ifstream input(filepath.c_str(), ios::in|ios::binary);
-	if (read_uint(input) != 440477507) {
-		input.close();
+	unsigned int i, j, k, chrcnt, chrlen, nblockcnt, maskblockcnt, rem;
+	unsigned char achar;
+	char len_chrname;
+	char chrname[256];
+	vector<unsigned int> nblockstarts;
+	vector<unsigned int> nblocksizes;
+	
+	FILE *input = fopen(filepath.c_str(), "r");
+	if (read_uint(input) != 440477507) { // Magic
+		fclose(input);
 		return 1;
 	}
-	if (read_uint(input) != 0) {
-		input.close();
+	if (read_uint(input) != 0) { // Version should be 0
+		fclose(input);
 		return 1;
 	}
 
-	unsigned int chrcnt = read_uint(input);
-	input.seekg(4, input.cur); // Reserved
-	input.close();
-	return 1;
+	chrcnt = read_uint(input);
+	fseek(input, 4, SEEK_CUR); // Reserved
+
+	for (i=0; i<chrcnt; i++) {
+		fread(&len_chrname, 1, 1, input);
+		fread(chrname, 1, len_chrname, input);
+		chrname[len_chrname] = 0;
+		chrnames.push_back(string(chrname));
+		fseek(input, 4, SEEK_CUR); // Absolute position of each sequence
+	}
+
+	chrpos.push_back(0);
+	for (i=0; i<chrcnt; i++) {
+		chrlen = read_uint(input);
+		nblockcnt = read_uint(input);
+		for (j=0; j<nblockcnt; j++) nblockstarts.push_back(read_uint(input));
+		for (j=0; j<nblockcnt; j++) nblocksizes.push_back(read_uint(input));
+		maskblockcnt = read_uint(input);
+		fseek(input, maskblockcnt*8 + 4, SEEK_CUR);
+		char *chrbuf = new char[chrlen+1]; chrbuf[chrlen] = 0;
+		rem = chrlen&3;
+		for (j=0; j<chrlen-rem; j+=4) {
+			fread((char*)&achar, 1, 1, input);
+			for (k=0; k<4; k++)
+				chrbuf[j+k] = bit_to_seq((achar>>((3-k)*2))&0x3);
+		}
+		fread((char*)&achar, 1, 1, input);
+		for (j=0; j<rem; j++)
+			chrbuf[chrlen-rem+j] = bit_to_seq((achar>>((3-j)*2))&0x3);
+		for (j=0; j<nblockcnt; j++)
+			for (k=nblockstarts[j]; k<nblockstarts[j]+nblocksizes[j]; k++)
+				chrbuf[k] = 'N';
+		content += chrbuf;
+		delete [] chrbuf;
+		if (i < chrcnt-1) {
+			content += ';';
+			chrpos.push_back(content.size());
+		}
+	}
+	fclose(input);
+	return 0;
 }
